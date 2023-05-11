29dcad574b630172c69ebc4736043bca4e169e0b
flaviovitale02 <flaviovitale73@gmail.com>
2023-05-11 17:55:55 +0200

    esercizio finito

---
+++ script.js
@@ -0,0 +1,7 @@
+const userName = prompt ('Please write your name')
+
+const userSurname = prompt ('Please write your username')
+
+const userFavColor = prompt ('Please write your favourite color')
+
+document.getElementById('password').innerHTML = userName + userSurname + userFavColor + 21
